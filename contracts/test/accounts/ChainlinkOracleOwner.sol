a713a554c27fac6f75a27a1c7c3ec99e09aa0ddf
Lucas Manuel <lucas@maple.finance>
2021-08-12 15:37:11 -0400

    chore: Update `main` branch for consumption (#5)
    * feat: add config file
    
    * feat: Break out `chainlink-oracle` directory in maple-core to be a submodule dapptools project (SC-3050) (#1)
    
    * refactor: make contract compilable
    
    * fix: address review comments
    
    * fix: remove redundant submodule
    
    * fix: add globals variable back
    
    * fix: mock the aggregator
    
    * fix: address feedback comments
    
    * fix: styling fixes
    
    * fix: stying fixes
    
    * feat: update ci config (#2)
    
    * feat: Make Release (#3)
    
    * feat: Better Make Release (#4)
    
    - and remove unneeded module
    - and take latest oracle
    
    Co-authored-by: Lucas Manuel <lucasmanuel@MacBook-Pro-2.local>
    Co-authored-by: Satyam Agrawal <satyam0499@gmail.com>
    Co-authored-by: Michael De Luca <35537333+deluca-mike@users.noreply.github.com>

---
+++ contracts/test/accounts/ChainlinkOracleOwner.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import { IChainlinkOracle } from "../../interfaces/IChainlinkOracle.sol";
+
+contract ChainlinkOracleOwner {
+
+    function setManualPrice(address target, int256 price) external { 
+        IChainlinkOracle(target).setManualPrice(price); 
+    }
+
+    function setManualOverride(address target, bool _override) external {
+        IChainlinkOracle(target).setManualOverride(_override); 
+    }
+
+    function changeAggregator(address target, address aggregator) external {
+        IChainlinkOracle(target).changeAggregator(aggregator); 
+    }
+
+    function try_chainlinkOracle_setManualPrice(address oracle, int256 priceFeed) external returns (bool ok) {
+        string memory sig = "setManualPrice(int256)";
+        (ok,) = oracle.call(abi.encodeWithSignature(sig, priceFeed));
+    }
+
+    function try_chainlinkOracle_setManualOverride(address oracle, bool _override) external returns (bool ok) {
+        string memory sig = "setManualOverride(bool)";
+        (ok,) = oracle.call(abi.encodeWithSignature(sig, _override));
+    }
+
+    function try_chainlinkOracle_changeAggregator(address oracle, address aggregator) external returns (bool ok) {
+        string memory sig = "changeAggregator(address)";
+        (ok,) = oracle.call(abi.encodeWithSignature(sig, aggregator));
+    }
+
+}
