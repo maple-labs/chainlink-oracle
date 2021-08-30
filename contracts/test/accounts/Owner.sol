2f5e9057929156ef5f23230fe8224fd8ee7e7515
Michael De Luca <michael.deluca@circle-free.com>
2021-08-30 19:25:36 -0400

    feat: Complete Test Accounts (SC-3610)
    - and remove interfaces from interface

---
+++ contracts/test/accounts/Owner.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import { Ownable } from "../../../modules/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+import { IChainlinkOracle } from "../../interfaces/IChainlinkOracle.sol";
+
+contract Owner {
+
+    /************************/
+    /*** Direct Functions ***/
+    /************************/
+
+    function chainlinkOracle_changeAggregator(address oracle, address aggregator) external {
+        IChainlinkOracle(oracle).changeAggregator(aggregator); 
+    }
+
+    function chainlinkOracle_setManualPrice(address oracle, int256 price) external { 
+        IChainlinkOracle(oracle).setManualPrice(price); 
+    }
+
+    function chainlinkOracle_setManualOverride(address oracle, bool _override) external {
+        IChainlinkOracle(oracle).setManualOverride(_override); 
+    }
+
+    function chainlinkOracle_renounceOwnership(address oracle) external {
+        Ownable(oracle).renounceOwnership(); 
+    }
+
+    function chainlinkOracle_transferOwnership(address oracle, address newOwner) external {
+        Ownable(oracle).transferOwnership(newOwner); 
+    }
+
+    /*********************/
+    /*** Try Functions ***/
+    /*********************/
+
+    function try_chainlinkOracle_changeAggregator(address oracle, address aggregator) external returns (bool ok) {
+        (ok,) = oracle.call(abi.encodeWithSelector(IChainlinkOracle.changeAggregator.selector, aggregator));
+    }
+
+    function try_chainlinkOracle_setManualPrice(address oracle, int256 priceFeed) external returns (bool ok) {
+        (ok,) = oracle.call(abi.encodeWithSelector(IChainlinkOracle.setManualPrice.selector, priceFeed));
+    }
+
+    function try_chainlinkOracle_setManualOverride(address oracle, bool _override) external returns (bool ok) {
+        (ok,) = oracle.call(abi.encodeWithSelector(IChainlinkOracle.setManualOverride.selector, _override));
+    }
+
+    function try_chainlinkOracle_renounceOwnership(address oracle) external returns (bool ok) {
+        (ok,) = oracle.call(abi.encodeWithSelector(Ownable.renounceOwnership.selector));
+    }
+
+    function try_chainlinkOracle_transferOwnership(address oracle, address newOwner) external returns (bool ok) {
+        (ok,) = oracle.call(abi.encodeWithSelector(Ownable.transferOwnership.selector, newOwner));
+    }
+
+}
